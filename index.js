2c4a2f597eeb071a862cf13398c20afafbebad7a
zenakhane <lilliantyiso@gmail.com>
2022-03-17 15:48:43 +0200

    highlight and hide

--- index.js
+++ index.js
@@ -5,37 +5,51 @@ const addBtnElem = document.querySelector('.btn');
 // display element
 const wordElem = document.querySelector('.words1');
 // counter element
-const counterElem = document.querySelector('.counter')
+const counterElem = document.querySelector('.counter');
 // checkbox element
-const checkWords = document.querySelector('.charac')
+const element = document.querySelector('.charac');
 
 
 
 
-function display(){
-        const checkBtn = document.querySelector("input[name='wordie']:checked");
-       const words = sentence.value;
-        const splitWords = words.split(" ");
-        let wordsDisplay = '';
-        for(var i = 0; i < splitWords.length; i++ ){
-          const wordsIndex = splitWords[i]
-
-        if( wordsIndex.length >= 5){
-          wordsDisplay += `<mark style ="background-color:CDFFF9;">${wordsIndex}</mark> `
-        } else{
-                 wordsDisplay += wordsIndex + ' '
-        } 
-}
-checkButton()
-        wordElem.innerHTML = wordsDisplay
-        counterElem.innerHTML = splitWords.length
-}
-addBtnElem.addEventListener('click',display)
-
-
-function checkButton(){
+const display = () => {
   const checkBtn = document.querySelector("input[name='wordie']:checked");
-  checkBtn.checked = false;
-  console.log('charac')
+  const words = sentence.value;
+  const splitWords = words.split(" ");
+  let wordsDisplay = '';
+  for (var i = 0; i < splitWords.length; i++) {
+    const wordsIndex = splitWords[i]
+
+    if (wordsIndex.length >= 5) {
+      wordsDisplay += `<mark style ="background-color:CDFFF9;">${wordsIndex}</mark> `
+    } else {
+      wordsDisplay += wordsIndex + ' '
+    }
+  }
+  wordElem.innerHTML = wordsDisplay
+  counterElem.innerHTML = splitWords.length
 }
-checkWords.addEventListener('click',checkButton)
+addBtnElem.addEventListener('click', display)
+
+
+const checkButton = () => {
+  const checkBtn = document.querySelector("input[name='wordie']:checked");
+  const words = sentence.value;
+  const splitWords = words.split(" ");
+  let wordsDisplay = '';
+
+  for (let i = 0; i < splitWords.length; i++) {
+    const element = splitWords[i];
+    if (element.length >= 5) {
+      checkBtn.style.display = 'none';
+      wordsDisplay += `<mark style ="background-color:CDFFF9;">${element}</mark> `
+    }
+    else {
+      checkBtn.checked = false;
+      console.log(checkBtn.checked)
+    }
+      wordElem.innerHTML = wordsDisplay
+
+  }
+}
+element.addEventListener('click', checkButton)
